1c7c9a0dd17d83899c66cf8b7436565c31985d1b
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-28 19:47:01 -0300

    update workflow formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,4 @@
 
 rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1
 
-echo "teste"
-
-sudo rit demo coffee-shell --rit_name=Dennis --rit_coffee_type=espresso --rit_delivery=false
-
-echo "sudo $2"
-
 sudo $2
