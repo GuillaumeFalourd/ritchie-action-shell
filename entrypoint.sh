d26c402e7dad8eaa29a503b1e0ce4a568d3f2f66
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-28 19:56:01 -0300

    update workflow formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,4 @@
 
 sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1
 
-echo "PATH -------------"
-find / -name ".rit"
-echo "/RIT -------------"
-ls -lha /root/.rit
-echo "GITHUB -------------"
-ls -lha /github/home
-echo "GITHUB RIT -------------"
-ls -lha /github/home/.rit
-
 sudo $2
