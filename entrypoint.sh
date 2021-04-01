91366adc234ddb772a0fc2e791ad1b606140ef60
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-01 15:13:29 -0300

    update token management

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
 # Add Github user credentials
-sudo rit set credential --provider=github --fields=username,token --values="$GITHUB_ACTOR","$3"
+sudo rit set credential --provider=github --fields=username,token --values="$GITHUB_ACTOR","$ACCESS_TOKEN"
 
 sudo rit list credential
 
 # Import formula repository command
-sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1 --token=$3
+sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1 --token=$ACCESS_TOKEN
 
 # Execute formula command
 sudo $2
