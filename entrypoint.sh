7317f06d973a9eb75e5efe75eaeeb756b8c576b2
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-01 14:36:13 -0300

    adding credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
+# Add Github user credentials
+sudo rit set credential --provider=github --fields=username,token --values="$GITHUB_ACTOR","$GITHUB_TOKEN"
+
+sudo rit list credential
+
 # Import formula repository command
 sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1
 
