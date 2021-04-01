4a24dbf9ac1c8e1475901b41890588997dfba858
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-01 14:38:34 -0300

    update entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ sudo rit set credential --provider=github --fields=username,token --values="$GIT
 sudo rit list credential
 
 # Import formula repository command
-sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1
+sudo rit add repo --provider=Github --name=imported-repo --repoUrl=$1 --priority=1 --token=$GITHUB_TOKEN
 
 # Execute formula command
 sudo $2
